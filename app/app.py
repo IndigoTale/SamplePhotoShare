48a11b9129643cf413f489ecda5069457052b95c
IndigoTale <paretto.619@gmail.com>
2019-04-12 00:39:17 +0900

    update

--- app/app.py
+++ app/app.py
@@ -4,11 +4,14 @@ import json
 app = Flask(__name__)   
 
 @app.route('/')                                   
-def hello_world():                                
+def index():                                
     return render_template("index.html")                       
 @app.route('/add_user')
 def add_user():
     return "Hello World!"  
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 if __name__ == '__main__':                        
     app.run(host="0.0.0.0", port=80, debug=True)
