c6bfbbcc3f78233420764f7834f03be1c8460c30
IndigoTale <paretto.619@gmail.com>
2019-04-11 15:47:48 +0900

    update index.html

--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
-from flask import Flask                           
+from flask import Flask,render_template                           
 import json
 
 app = Flask(__name__)   
 
 @app.route('/')                                   
 def hello_world():                                
-    return "Hello World!"                         
+    return render_template("index.html")                       
 @app.route('/add_user')
 def add_user():
     return "Hello World!"  
