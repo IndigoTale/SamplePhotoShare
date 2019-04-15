0b3e0cac2d0d62b01b97fd7e00e7ea417cdab95b
IndigoTale <paretto.619@gmail.com>
2019-04-15 20:27:10 +0900

    update

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect, session
 from flask_login import LoginManager, login_user, login_required
+from werkzeug.utils import secure_filename
 import json
 import lib
 from lib import *
@@ -58,22 +59,53 @@ def signup_form():
 
 @app.route('/signup', methods=["POST"])
 def signup():
-
     flag = lib.signUpCheck(request.form["username"], request.form["email"],
                            request.form["password"], request.form["repeat-password"])
     if flag[0]:
         session['user_id'] = request.form['email']
-   
+
         return redirect('https://photoshare.tk')
     else:
         return render_template('signup.html', code=flag[1])
-@app.route('/upload',methods=['GET'])
+
+
+@app.route('/upload', methods=['GET'])
 def upload_form():
+    if session.get('user_id') is not None:
+        res = queryUserIdTable(session.get('user_id'))
+        if res[0] is True:
+            return render_template('upload.html', username=res[2])
+        else:
+            return redirect("https://photoshare.tk/signup")
+    else:
+        return redirect("https://photoshare.tk/login")
+
     return render_template('upload.html')
-@app.route('/upload',methods=['POST'])
+
+
+@app.route('/upload', methods=['POST'])
 def upload():
-    if request.form.get('upload-comment'):
-        return request.form.get('upload-comment')
+    # 認証
+    if session.get('user_id') is not None:
+        res = queryUserIdTable(session.get('user_id'))
+        if res[0] is True:
+            user_id = res[0]
+            username = res[1]
+        else:
+            return redirect("https://photoshare.tk/signup")
+    else:
+        return redirect("https://photoshare.tk/login")
+    # フォームが揃っているか
+    if request.form.get('upload-title') and request.form.get('upload-comment') and request.file.get('upload-file'):
+        title = request.form.get('upload-title')
+        comment = request.form.get('upload-comment')
+        img_file = request.file.get('upload-file')
+        if img_file and allowed_file(img_file.filename):
+            filename = f"{uuid.uuid4().hex}.{img_file.filename.split('.')[1]}"            
+            if lib.upload_photo_to_s3(img_file,filename) and \
+                lib.upload_photo_info_to_dynamodb(filename,title,comment,user_id,username):
+                    return redirect(request.url)
+
     return "Fail"
 
 
--- app/lib.py
+++ app/lib.py
@@ -10,27 +10,31 @@ import uuid
 from datetime import datetime, timedelta, timezone
 JST = timezone(timedelta(hours=+9), 'JST')
 # 投稿可能な画像の拡張子
-ALLOWED_EXTENSIONS = ('png', 'jpeg')
+ALLOWED_EXTENSIONS = ('png', 'jpg', 'jpeg')
 
-
+# Flask App (Docker Container On EC2) からアクセスするのでセッションが必要
 with open("/app/aws_session_info.json", "r") as f:
     aws_session_info = json.load(f)
-
-if aws_session_info is None:
-    exit(1)
-
 aws_session = boto3.Session(
     aws_access_key_id=aws_session_info["ACCESS_KEY_ID"],
     aws_secret_access_key=aws_session_info["SECRET_ACCESS_KEY"],
     region_name=aws_session_info["REGION_NAME"]
 )
+# S3の情報
+with open("/app/aws_s3_info.json", "r") as f:
+    aws_s3_info = json.load(f)
+# S3操作用クラス
+photoBucket = aws_session.resource('s3').Bucket(aws_s3_info["BUCKET_NAME"])
 
-userIdTable = aws_session.resource(
-    'dynamodb').Table('userIdTable')
-userNameTable = aws_session.resource(
-    'dynamodb').Table('userNameTable')
-photoTable = aws_session.resource(
-    'dynamodb').Table('photoTable')
+
+if aws_session_info and aws_s3_info:
+    exit(1)
+
+# DynamoDBのテーブル操作用クラス
+userIdTable = aws_session.resource('dynamodb').Table('userIdTable')
+userNameTable = aws_session.resource('dynamodb').Table('userNameTable')
+photoTable = aws_session.resource('dynamodb').Table('photoTable')
+infoTable = aws_session.resource('dynamodb').Table('infoTable')
 
 
 def signUpCheck(username, email, password, repeat_password):
@@ -81,7 +85,7 @@ def signUpCheck(username, email, password, repeat_password):
         }
     )
     try:
-       userNameTable.put_item(
+        userNameTable.put_item(
             Item={
                 "userId": email,
                 "password": hashlib.sha256(password.encode()).hexdigest(),
@@ -180,6 +184,70 @@ def uploadPhoto(user_id, username):
     )
 
 
+def upload_photo_to_s3(file, filename):
+
+    try:
+        photoBucket.upload_fileobj(file, aws_s3_info['PHOTO_FOLDER']+filename)
+    except:
+        return False
+    else:
+        return True
+
+
+def upload_photo_info_to_dynamodb(filename, title, comment,user_id,username):
+    try:
+        res = infoTable.get_item(
+            Key={
+                "tableName": "photoTable"
+            }
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return False
+    else:
+        pass
+    if res.get('Item').get('photoCount'):
+        photo_count = str(int(res.get('Item').get('photoCount')) + 1)
+    else:
+        return False
+    try:
+        infoTable.update_item(
+            Key={
+                "tableName":"photoTable"
+            },
+            UpdateExpression="set photoCount = :p",
+            ExpressionAttributeValues={
+                ":p":photo_count
+            }
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return False
+    else:
+        pass
+    try:
+        upload_date = datetime.now(JST)
+        photoTable.put_item(
+            Item={
+                "photoCount":photo_count,
+                "userId": user_id,
+                "userName": username,
+                "uploadDate": upload_date.strftime('%Y-%m-%d %H:%M:%S'),
+                "uploadYear": upload_date.strftime('%Y'),
+                "uploadMonth": upload_date.strftime('%m'),
+                "uploadDay": upload_date.strftime('%d'),
+            }
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return False
+    else:
+        pass
+        
+    return True
+
+    
+
 def updatePhoto(photo_id, user_id):
     try:
         res = photoTable.get_item(
@@ -191,3 +259,8 @@ def updatePhoto(photo_id, user_id):
         print(e.response['Error']['Message'])
     else:
         pass
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
