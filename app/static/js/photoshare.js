73b259433841425d000f17d2606a08461876ed00
IndigoTale <paretto.619@gmail.com>
2019-04-19 00:22:29 +0900

    update

--- app/static/js/photoshare.js
+++ app/static/js/photoshare.js
@@ -12,11 +12,18 @@ function clickHeartButton(photo_id){
             if(res.push == true){
                 var photoFrame = document.getElementById(photo_id);
                 photoFrame.getElementsByTagName('span')[1].getElementsByTagName('button')[0].getElementsByTagName('img')[0].src = 'static/img/icon-heart-pink.png'
+            var count = photoFrame.getElementsByTagName('span')[1].getElementsByTagName('button')[0].textContent
+                count = Integer(count) + 1
+                photoFrame.getElementsByTagName('span')[1].getElementsByTagName('button')[0].textContent = String(count)    
+
                 console.log(photoFrame);
             }
             else{
                 var photoFrame = document.getElementById(photo_id);
                 photoFrame.getElementsByTagName('span')[1].getElementsByTagName('button')[0].getElementsByTagName('img')[0].src = 'static/img/icon-heart.png'
+                var count = photoFrame.getElementsByTagName('span')[1].getElementsByTagName('button')[0].textContent
+                count = Integer(count) - 1
+                photoFrame.getElementsByTagName('span')[1].getElementsByTagName('button')[0].textContent = String(count)
             }
         }   
     }
